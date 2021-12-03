b16d9f717f223c9e2f44dee6cd601328bbae8b59
Piotr Korbas <piotrkorbas@gmail.com>
2021-12-03 18:19:28 +0100

    strona tajemnicze mapy

--- index.js
+++ index.js
@@ -42,6 +42,21 @@ function accordion(id) {
   }
 }
 
+/*----------- tabs------------*/
+function openTab(evt, TabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(TabName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
 /*----------- scroll to ------------*/
 // var mybutton = document.getElementById("myBtn");
 
