1b5b018b54ef4118a9d3e38184b58b2aeea61239
Piotr Korbas <piotrkorbas@gmail.com>
2021-12-18 11:11:53 +0100

    menu scrolowane

--- index.js
+++ index.js
@@ -33,6 +33,68 @@ function subMenuEvents() {
   }
 }
 
+// $(window).bind('scroll', function (){
+//   if ($window.scrollTop()> 500) {
+//     $('.nav-mobile-container').addClass('scroll-up');
+//   }
+//   else {
+//     $('.nav-mobile-container').removeClass('scroll-up');
+//   }
+// });
+(function(){
+
+  var doc = document.documentElement;
+  var w = window;
+
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curScroll;
+  var direction = 0;
+  var prevDirection = 0;
+
+  var header = document.getElementById('site-header');
+
+  var checkScroll = function() {
+
+    /*
+    ** Find the direction of scroll
+    ** 0 - initial, 1 - up, 2 - down
+    */
+
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) { 
+      //scrolled up
+      direction = 2;
+    }
+    else if (curScroll < prevScroll) { 
+      //scrolled down
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
+
+    prevScroll = curScroll;
+  };
+
+  var toggleHeader = function(direction, curScroll) {
+    if (direction === 2 && curScroll > 52) { 
+
+      //replace 52 with the height of your header in px
+
+      header.classList.add('hide');
+      prevDirection = direction;
+    }
+    else if (direction === 1) {
+      header.classList.remove('hide');
+      prevDirection = direction;
+    }
+  };
+
+  window.addEventListener('scroll', checkScroll);
+
+})();
+
 /*----------- accordion------------*/
 function accordion(id) {
   var show = document.getElementById(id);
